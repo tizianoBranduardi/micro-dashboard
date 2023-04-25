229a07bcb82c1df53730333ea8e84d59514f5c9e
Tiziano Branduardi <tiziano.branduardi@gmail.com>
2023-04-25 19:03:37 +0200

    Added connection to db container and first print

--- app/app.py
+++ app/app.py
@@ -2,6 +2,13 @@ import os
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+import psycopg2
+
+conn = psycopg2.connect(
+    host="micro-dashboard_db_1",
+    database="micro_dashboard",
+    user="postgres",
+    password="postgres")
 
 debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
 
@@ -9,23 +16,27 @@ app = Dash(__name__)
 
 server = app.server
 
-data = pd.DataFrame(
-    {
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-    }
-)
+# data = pd.DataFrame(
+#     {
+#         "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+#         "Amount": [4, 1, 2, 2, 4, 5],
+#         "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+#     }
+# )
+data = pd.read_sql('SELECT * FROM articolo LIMIT 5', conn)
+print(data)
 
-graph = px.bar(data, x="Fruit", y="Amount", color="City", barmode="group")
+
+#graph = px.bar(data, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(
     children=[
         html.H1(
-            children=f"Hello Dash in 2023 from {'Dev Server' if debug else 'Prod Server'}"
+            children=f"Hello Dash from {'Dev Server' if debug else 'Prod Server'}"
         ),
         html.Div(children="""Dash: A web application framework for your data."""),
-        dcc.Graph(id="example-graph", figure=graph),
+        html.Div(data.to_html()),
+        #dcc.Graph(id="example-graph", figure=graph),
     ]
 )
 
