13e4989a3659325d1e90293b580dcd61c4ce85e0
Tiziano Branduardi <tiziano.branduardi@gmail.com>
2023-10-05 00:00:37 +0200

    Added new pages

---
+++ app/pages/fatturatoPerFamiglia.py
@@ -0,0 +1,33 @@
+import os
+import dash
+from dash import Dash, html, dcc, dash_table
+import plotly.express as px
+import pandas as pd
+from utils.db_connector import conn, query_fatturato_famiglia
+from dateutil.parser import parse
+
+dash.register_page(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+
+fatturato_per_famiglia = pd.read_sql(query_fatturato_famiglia, conn)
+
+layout = html.Div(
+    children=[
+        html.Br(),
+        html.H4(
+            children="Fatturato per settore"
+        ),
+        dash_table.DataTable(fatturato_per_famiglia.to_dict('records'),
+                            [{"name": i, "id": i} for i in fatturato_per_famiglia.columns],
+                            sort_action="native",
+                            sort_mode='multi',
+                            filter_action="native",
+                            filter_options={"placeholder_text": "Filter column..."},
+                            page_size=20,),
+    ]
+)
--- app/utils/db_connector.py
+++ app/utils/db_connector.py
@@ -14,3 +14,39 @@ query_homepage = 'SELECT F.codice, F.data, C.ragione_sociale, A.codice, A.descri
                 JOIN articolo_in_fattura AS A_F ON F.data = A_F.data_fattura_fk AND F.codice = A_F.codice_fattura_fk\
                 JOIN articolo AS A ON A.codice = A_F.codice_articolo_fk AND A.descrizione = A_F.descrizione_articolo_fk\
                 JOIN cliente AS C ON F.cliente_fk = C.codice_bms'
+
+query_fatturato_cliente = 'WITH temp as (SELECT C.ragione_sociale, A_F.quantita * A_F.prezzo as total\
+                FROM public.fattura AS F\
+                JOIN public.articolo_in_fattura AS A_F ON F.data = A_F.data_fattura_fk AND F.codice = A_F.codice_fattura_fk\
+                JOIN public.articolo AS A ON A.codice = A_F.codice_articolo_fk AND A.descrizione =A_F.descrizione_articolo_fk\
+                JOIN public.cliente AS C ON F.cliente_fk = C.codice_bms)\
+                SELECT ragione_sociale, SUM(total) as total\
+                FROM temp AS T\
+                GROUP BY T.ragione_sociale'
+
+query_fatturato_settore = 'WITH temp as (SELECT C.settore, A_F.quantita * A_F.prezzo as total\
+                        FROM public.fattura AS F\
+                        JOIN public.articolo_in_fattura AS A_F ON F.data = A_F.data_fattura_fk AND F.codice = A_F.codice_fattura_fk\
+                        JOIN public.articolo AS A ON A.codice = A_F.codice_articolo_fk AND A.descrizione =A_F.descrizione_articolo_fk\
+                        JOIN public.cliente AS C ON F.cliente_fk = C.codice_bms)\
+                        SELECT settore, SUM(total) as total\
+                        FROM temp AS T\
+                        GROUP BY T.settore'
+
+query_fatturato_paese = 'WITH temp as (SELECT C.paesi, A_F.quantita * A_F.prezzo as total\
+                        FROM public.fattura AS F\
+                        JOIN public.articolo_in_fattura AS A_F ON F.data = A_F.data_fattura_fk AND F.codice = A_F.codice_fattura_fk\
+                        JOIN public.articolo AS A ON A.codice = A_F.codice_articolo_fk AND A.descrizione =A_F.descrizione_articolo_fk\
+                        JOIN public.cliente AS C ON F.cliente_fk = C.codice_bms)\
+                        SELECT paesi, SUM(total) as total\
+                        FROM temp AS T\
+                        GROUP BY T.paesi'
+
+query_fatturato_famiglia = 'WITH temp as (SELECT A.famiglia, A_F.quantita * A_F.prezzo as total\
+                            FROM public.fattura AS F\
+                            JOIN public.articolo_in_fattura AS A_F ON F.data = A_F.data_fattura_fk AND F.codice = A_F.codice_fattura_fk\
+                            JOIN public.articolo AS A ON A.codice = A_F.codice_articolo_fk AND A.descrizione =A_F.descrizione_articolo_fk\
+                            JOIN public.cliente AS C ON F.cliente_fk = C.codice_bms)\
+                            SELECT famiglia, SUM(total) as total\
+                            FROM temp AS T\
+                            GROUP BY T.famiglia'
