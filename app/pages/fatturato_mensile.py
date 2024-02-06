b497eef499310e5fdc45babb8d13b5419467083c
Tiziano Branduardi <tiziano.branduardi.ext@lastminute.com>
2024-02-06 09:29:48 +0100

    Added percentage comparison with past years

--- app/pages/fatturato_mensile.py
+++ app/pages/fatturato_mensile.py
@@ -4,6 +4,8 @@ import plotly.express as px
 from dash import html, dcc, dash_table, callback, Output, Input
 from utils.db_connector import engine, query_fatturato
 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 dash.register_page(__name__)
 
 colors = {
@@ -14,7 +16,13 @@ colors = {
 sales = pd.read_sql(query_fatturato, engine)
 sales['data'] = pd.to_datetime(sales['data'], format='%Y/%m/%d', utc=False)
 
-dates = sales["data"].sort_values().unique()
+dates = sales
+dates['month'] = dates['data'].dt.month
+dates['year'] = dates['data'].dt.year
+dates = (dates[['month', 'year']]
+         .drop_duplicates(ignore_index=True)
+         .sort_values(by=['year', 'month'])
+         .reset_index(drop=True))
 
 sales_mod = sales[['data', 'total']]
 sales_mod['day'] = sales_mod['data'].dt.day
@@ -54,21 +62,6 @@ layout = html.Div(
         html.H4(
             children="Analisi Fatturato - Scope mese"
         ),
-        # html.Div(
-        #     children=[
-        #         html.Div(
-        #             children="Mesi"  # , className="menu-title"
-        #         ),
-        #         html.Br(),
-        #         dcc.DatePickerRange(
-        #             id="date-range",
-        #             min_date_allowed=sales["data"].min(),
-        #             max_date_allowed=sales["data"].max(),
-        #             start_date=sales["data"].min(),
-        #             end_date=sales["data"].max(),
-        #         ),
-        #     ]
-        # ),
         html.Br(),
         html.Div(
             children=[
@@ -92,30 +85,77 @@ layout = html.Div(
             ],
             className="wrapper",
         ),
+        html.Br(),
+        html.Div(
+            children=[
+                html.H4("Confronto percentuale"),
+                html.P("Selezionare l'anno di riferimento"),
+                html.Br(),
+                dcc.Slider(
+                    id='year_slider',
+                    dots=True,
+                    #value=dates['year'].min(),
+                    min=dates['year'].min(),
+                    max=dates['year'].max(),
+                    step=1,
+                    tooltip={"placement": "bottom", "always_visible": True},
+                    marks=None
+                ),
+                html.Br(),
+                html.Div(
+                    children=dcc.Graph(
+                        id="sales_graph_month",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Br()
+            ],
+            className="wrapper",
+        ),
     ]
 )
 
-# @callback(
-#     Output("sales_graph_month", "figure"),
-#     [Input("date-range", "start_date"),
-#      Input("date-range", "end_date")]
-# )
-# def update_sale_graph(start_date, end_date):
-#     if not start_date or not end_date:
-#         raise dash.exceptions.PreventUpdate
-#     else:
-#         mask = (sales['data'] >= start_date) & (sales['data'] <= end_date)
-#         sales_filtered = sales.loc[mask]
-#         sales_filtered = sales_filtered[['data', 'total']]
-#         sales_filtered['day'] = sales_filtered['data'].dt.day
-#         sales_filtered['month'] = sales_filtered['data'].dt.month
-#         sales_filtered['year'] = sales_filtered['data'].dt.year
-#         sales_per_month_filtered = (sales_filtered.groupby(['year', 'month'])['total'].sum()
-#                                     .reset_index()
-#                                     .rename(columns={'year': 'Anno', 'month': 'Mese', 'total': 'Totale'}))
-#
-#         return px.line(sales_per_month_filtered,
-#                        title="Fatturato",
-#                        x='Mese',
-#                        y='Totale',
-#                        color='Anno').update_xaxes(type='category')
+
+@callback(
+    Output("sales_graph_month", "figure"),
+    Input("year_slider", "value")
+)
+def update_sale_reference_graph(value):
+    if not value:
+        figure = px.line(sales_per_month,
+                         title="Fatturato mensile - confronto percentuale",
+                         #hover_name=sales_compared['Differenza'],
+                         x='Mese',
+                         y='Totale',
+                         color='Anno',
+                         markers=True).update_xaxes(type='category')
+        return figure
+    else:
+        sales_compared = sales_per_month
+        sales_compared['Differenza'] = ''
+        sales_selected_year = sales_compared.loc[sales_compared['Anno'] == value]
+        for index, row in sales_compared.iterrows():
+            if (int(row['Mese']) in list(sales_selected_year['Mese'])) \
+                    & (int(row['Anno']) != value):
+                reference_value = \
+                    sales_selected_year.loc[sales_selected_year['Mese'] == int(row['Mese'])]['Totale'].iloc[0]
+                percentage = (row['Totale'] / reference_value) - 1
+                percentage = " {:.2%}".format(percentage) if percentage < 0 else " +{:.2%}".format(percentage)
+                sales_compared.loc[index, 'Differenza'] = "{} rispetto a {}".format(percentage, value)
+        sales_compared.sort_values(by=['Mese', 'Anno'], inplace=True)
+        figure = px.line(sales_compared,
+                         title="Fatturato mensile - confronto percentuale",
+                         hover_name=sales_compared['Differenza'],
+                         x='Mese',
+                         y='Totale',
+                         color='Anno',
+                         markers=True).update_xaxes(type='category')
+        for line in figure.data:
+
+            if line.name != str(value):
+                line.line.width = 1
+            else:
+                line.line.width = 4
+
+        return figure
